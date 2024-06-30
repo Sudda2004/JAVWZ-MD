bb9fd0e0d3644ccc5b2c437d07d7e7648a6b3f9c
Sudda2004 <slsudda154353@gmail.com>
2024-06-30 02:54:52 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,8 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || '[6/30/2024 2:45 AM] no name: VAJIRA-MD=HcclDbTC#2Alj7vRC9hGXBGdxpu-ioBdeZOBYVuONSWU71Ji7Eys 
+youre session id' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
